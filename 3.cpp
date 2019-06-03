5c1966eded25709b236630aea94f402a3b5eb162
ctf11 <1796350803@qq.com>
2019-06-03 20:03:28 +0800

    leetcode 3. 无重复字符的最长子串

---
+++ 3.cpp
@@ -0,0 +1,22 @@
+//3. 无重复字符的最长子串
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+       map<char,int> mymap;
+        map<char,int>::iterator iter;
+        int n = 0,max=0,j=0;
+        for(int i=0;i<s.length();i++)
+        {
+	        if(mymap.find(s[i])!= mymap.end()&&mymap[s[i]]>=j)
+            {
+	          n =n - mymap[s[i]]+j-1;
+              j= mymap[s[i]]+1;  
+            }
+          mymap[s[i]]=i;
+	   n++;
+	   if(n>max)
+          max=n;
+        }
+        return max;
+    }
+};
